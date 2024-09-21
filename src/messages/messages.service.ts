65362d1d7eb82388bbd0a1aaf873b094e1f871c7
Richard Omoyele <109069693+Richiee-D@users.noreply.github.com>
2024-09-21 12:29:20 +0100

    Dependency injections manually added

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -2,21 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { CreateMessageDto } from './dto/create-message.dto';
 import { UpdateMessageDto } from './dto/update-message.dto';
 import { MessagesRepository } from './messages.repository';
-
 @Injectable()
 export class MessagesService {
-  messagesRepo: MessagesRepository;
-
-  constructor() {
-    //Service is creating its own dependency
-    //Do not do this in real life
-    this.messagesRepo = new MessagesRepository();
-  }
-  create(content: CreateMessageDto) {
+  constructor(private messagesRepo: MessagesRepository) {}
+  async create(content: CreateMessageDto) {
     return this.messagesRepo.create(content.content);
   }
 
-  findAll() {
+  async findAll() {
     return this.messagesRepo.findAll();
   }
 
